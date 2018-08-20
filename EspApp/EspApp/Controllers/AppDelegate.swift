d71671cc838ea2176f5033d2079151f71eaf13c2
JOAO LOPES <joaolopesf@gmail.com>
2018-08-20 15:03:12 -0300

    Version 0.1.2 updates

--- EspApp/EspApp/Controllers/AppDelegate.swift
+++ EspApp/EspApp/Controllers/AppDelegate.swift
@@ -61,7 +61,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     //         application to it. This property is optional since there are legitimate
     //         error conditions that could cause the creation of the store to fail.
     //        */
-    //        let container = NSPersistentContainer(name: "Relax_Mind")
+    //        let container = NSPersistentContainer(name: "EspApp")
     //        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
     //            if let error = error as NSError? {
     //                // Replace this implementation with code to handle the error appropriately.
